7d5f2c29547eeb3fcb57dcbeea34c40e13458826
Kelechi Nwachukwu <caesey2001@yahoo.com>
2022-09-23 17:55:10 -0400

    Add StreamProcessor to viz

--- savia/vizualizer.py
+++ savia/vizualizer.py
@@ -1,17 +1,37 @@
-import pandas as pd 
-import matplotlib.pyplot as plt 
-import numpy as py 
+from dataclasses import dataclass, field
 
-def truth_viz(perceived truth, 
-ground_truth =None, spacing=5, ratio=1/5,
-offset =10, image=False):
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
 
 
-    cat_pos = {cat: (i*3)+2 for i, cat in 
-    enumerate(perceived_truth["Category 
-    Tag"].unique())}
+@dataclass
+class StreamProcessor:
+    """StreamProcessor"""
 
-    x = np.linspace(1, int(perceived_truth
-    ["Time (Seconds)"].max()), int
-    (perceived_truth["Time (Seconds)"].max
-    ()/spacing))
+    frame_store: list = field(default_factory=list)
+    frame_gray_store: list = field(default_factory=list)
+    frame_hist_store: list = field(default_factory=list)
+    frame_intensity_store: list = field(default_factory=list)
+
+    frame_count: int = None
+
+    events: list = field(default_factory=list)
+    uri: str = None
+    fps: int = 24
+
+
+def truth_viz(
+    perceived_truth, ground_truth=None, spacing=5, ratio=1 / 5, offset=10, image=False
+):
+
+    cat_pos = {
+        cat: (i * 3) + 2
+        for i, cat in enumerate(perceived_truth["Category Tag"].unique())
+    }
+
+    x = np.linspace(
+        1,
+        int(perceived_truth["Time (Seconds)"].max()),
+        int(perceived_truth["Time (Seconds)"].max() / spacing),
+    )
